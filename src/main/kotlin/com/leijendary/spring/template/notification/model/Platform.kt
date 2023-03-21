01116481525bc35bbae513033280a2c99e61ae3b
leijendary <jonathanleijendekker@gmail.com>
2023-03-21 21:52:14 +0100

    removed toString

--- src/main/kotlin/com/leijendary/spring/template/notification/model/Platform.kt
+++ src/main/kotlin/com/leijendary/spring/template/notification/model/Platform.kt
@@ -4,10 +4,4 @@ enum class Platform(val value: String) {
     IOS("ios"),
     ANDROID("android"),
     WEB("web");
-
-    companion object {
-        fun from(value: String) = values().first { it.value == value }
-    }
-
-    override fun toString() = value
 }
