b5ceb94a46a55253ae4e343076b82e5d1642afe2
leijendary <jonathanleijendekker@gmail.com>
2023-08-04 15:50:26 +0200

    Renamed NotificationConsumer.kt to NotificationMessageConsumer

--- src/main/kotlin/com/leijendary/spring/template/notification/message/NotificationMessageConsumer.kt
+++ src/main/kotlin/com/leijendary/spring/template/notification/message/NotificationMessageConsumer.kt
@@ -15,21 +15,21 @@ import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class NotificationConsumer(
+class NotificationMessageConsumer(
     private val emailClient: EmailClient,
     private val notificationService: NotificationService,
     private val smsClient: SmsClient
 ) {
     @KafkaListener(topics = ["\${spring.kafka.topic.$NOTIFICATION_EMAIL.name}"])
     fun email(json: String) {
-        val emailMessage: EmailMessage = json.toClass()
+        val emailMessage = json.toClass<EmailMessage>()
 
         emailClient.send(emailMessage.to, emailMessage.templateId, emailMessage.parameters)
     }
 
     @KafkaListener(topics = ["\${spring.kafka.topic.$NOTIFICATION_PUSH.name}"])
     fun push(json: String) {
-        val pushMessage: PushMessage = json.toClass()
+        val pushMessage = json.toClass<PushMessage>()
         val notification = NotificationMapper.INSTANCE.toEntity(pushMessage)
 
         notificationService.create(notification)
@@ -37,7 +37,7 @@ class NotificationConsumer(
 
     @KafkaListener(topics = ["\${spring.kafka.topic.$NOTIFICATION_SMS.name}"])
     fun sms(json: String) {
-        val smsMessage: SmsMessage = json.toClass()
+        val smsMessage = json.toClass<SmsMessage>()
 
         smsClient.send(smsMessage.to, smsMessage.message)
     }
