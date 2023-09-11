0f943bfd45d41e7fee26c634cbfefb39d58adaf2
leijendary <jonathanleijendekker@gmail.com>
2023-09-11 22:59:42 +0200

    Added default serial version UID

--- src/main/kotlin/com/leijendary/spring/template/notification/core/entity/AppEntity.kt
+++ src/main/kotlin/com/leijendary/spring/template/notification/core/entity/AppEntity.kt
@@ -2,4 +2,9 @@ package com.leijendary.spring.template.notification.core.entity
 
 import java.io.Serializable
 
-open class AppEntity : Serializable
+open class AppEntity : Serializable {
+    companion object {
+        @JvmStatic
+        val serialVersionUID: Long = -1L
+    }
+}
--- src/main/kotlin/com/leijendary/spring/template/notification/core/model/Response.kt
+++ src/main/kotlin/com/leijendary/spring/template/notification/core/model/Response.kt
@@ -2,4 +2,9 @@ package com.leijendary.spring.template.notification.core.model
 
 import java.io.Serializable
 
-sealed interface Response : Serializable
+sealed interface Response : Serializable {
+    companion object {
+        @JvmStatic
+        val serialVersionUID: Long = -1L
+    }
+}
