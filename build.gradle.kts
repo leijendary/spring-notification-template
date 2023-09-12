6a2a4cf778a3cf79c6d9488ec2f26f02af934854
leijendary <jonathanleijendekker@gmail.com>
2023-09-12 11:36:14 +0200

    Made build simpler and fixed cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,14 +135,6 @@ tasks {
         options.compilerArgs.add("--enable-preview")
     }
 
-    bootJar {
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
-    }
-
-    jar {
-        enabled = false
-    }
-
     test {
         jvmArgs = listOf("--enable-preview")
         useJUnitPlatform()
