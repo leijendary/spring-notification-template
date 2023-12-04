12b35dac8d7a43de0e784a7b06554fc174c56b57
leijendary <jonathanleijendekker@gmail.com>
2023-12-04 19:51:10 +0100

    Updated EmailMessage

--- src/main/kotlin/com/leijendary/spring/template/notification/model/EmailMessage.kt
+++ src/main/kotlin/com/leijendary/spring/template/notification/model/EmailMessage.kt
@@ -1,3 +1,3 @@
 package com.leijendary.spring.template.notification.model
 
-data class EmailMessage(val to: String, val templateId: String, val parameters: Map<String, String>)
+data class EmailMessage(val to: String, val templateId: String, val parameters: Map<String, Any>)
